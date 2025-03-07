2804f446fe94d2f8c96a0bc7f94c906fa120ae2f
i10416 <ito.yo16uh90616@gmail.com>
2025-03-07 12:56:43 +0900

    chore: remove unused code

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,13 +1,5 @@
 package com.example
 
-// object A and object A$ at the same time => [error] produce classes that overwrite one another
-
-// - A.class
-// - A.tasty
-// - A$.class: module class of A
-// object A
-//
-// ---------------------------------------------------------------
 // - A$.class: class of A$
 // - A$.tasty
 // - A$$.class: module class of A$
